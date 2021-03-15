b981118d8d3942189fc57f547e1fa83b49670567
joshddunn <joshdunn.uwo@gmail.com>
2021-03-15 00:38:44 -0400

    love2d template project

--- conf.lua
+++ conf.lua
@@ -1,9 +1,9 @@
 function love.conf(t)
   t.version = "11.2"
 
-  t.window.title = "gems.love"
+  t.window.title = "Love2D Project"
   t.window.resizable = false
   t.window.vsync = false
-  t.window.width = 576
-  t.window.height = 576
+  t.window.width = 480
+  t.window.height = 270
 end
